b94a47b62fd70b65ca4b71cebe53e7455a015f1a
SBSSAHEL <sahlshanbin@gmail.com>
2026-02-13 23:55:03 +0600

    new

---
+++ A_Next_Round.cs
@@ -0,0 +1,23 @@
+string[] input = Console.ReadLine().Split();
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+
+string[] scoreIpt = Console.ReadLine().Split();
+int[] scores = new int[n];
+for (int i = 0; i < n; i++)
+{
+    scores[i] = int.Parse(scoreIpt[i]);
+}
+int kthSc = scores[k - 1];
+
+int cnt = 0;
+for (int i = 0; i < n; i++)
+{
+    if (scores[i] >= kthSc && scores[i] > 0)
+    {
+        cnt++;
+    }
+}
+
+Console.WriteLine(cnt);
